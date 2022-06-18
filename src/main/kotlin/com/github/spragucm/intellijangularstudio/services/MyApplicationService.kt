26d2048dcb23ca70cec7e08c05a827ba82aa8f75
GitHub Action <action@github.com>
2022-06-18 05:33:44 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/spragucm/intellijangularstudio/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.spragucm.intellijangularstudio.services
+
+import com.github.spragucm.intellijangularstudio.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
