d3f2ebfdc2c04d5e72a334592f018bd92548b273
Chris Sprague <spragucm@gmail.com>
2022-06-23 22:02:04 -0700

    Run script located at a given path

--- src/main/kotlin/com/tcubedstudios/angularstudio/terminal/actions/RunCommandInLocalTerminalAction.kt
+++ src/main/kotlin/com/tcubedstudios/angularstudio/terminal/actions/RunCommandInLocalTerminalAction.kt
@@ -1,8 +1,10 @@
 package com.tcubedstudios.angularstudio.terminal.actions
 
+import com.intellij.ide.plugins.PluginManager
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.terminal.JBTerminalWidget
@@ -31,16 +33,22 @@ class RunCommandInLocalTerminalAction: DumbAwareAction() {
             null -> LOGGY.error("Cannot run command in local terminal. Project is null")
             else -> {
                 try {
+                    val pluginPath = PluginManager.getPlugin(PluginId.getId("com.tcubedstudios.angularstudio"))?.path
+                    val scriptsRootPath = "$pluginPath\\scripts" //"I:\\Google Drive (HoverDroids)\\Code\\Windows\\Scripts"
+                    val scriptPath = "$scriptsRootPath\\Angular\\CreateWorkspaceWithMultipleProjectsAndLibraries.ps1"
+                    val scriptArguments = "-skipInstall true"
+
                     val terminalView = TerminalView.getInstance(project)
                     val window = ToolWindowManager.getInstance(project).getToolWindow(TerminalToolWindowFactory.TOOL_WINDOW_ID)
 
                     val contentManager = window?.contentManager
-                    val terminalWorkingDirectory = "I:\\Google Drive (HoverDroids)\\Code\\Windows\\Scripts"//project.basePath
+                    val terminalWorkingDirectory = project.basePath
+
                     val widget = when (val content = contentManager?.findContent(TAB_NAME)) {
                         null -> terminalView.createLocalShellWidget(terminalWorkingDirectory, TAB_NAME)
                         else -> TerminalView.getWidgetByContent(content) as ShellTerminalWidget
                     }
-                    val cmd =  "& \".\\Angular\\CreateWorkspaceWithMultipleProjectsAndLibraries.ps1\" -skipInstall true"
+                    val cmd =  "& \"$scriptPath\" $scriptArguments"
                     widget.executeCommand(cmd)
 
                 } catch (e: IOException) {
