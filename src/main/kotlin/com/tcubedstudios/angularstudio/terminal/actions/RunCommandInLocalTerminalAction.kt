8a4c3626e92808ec77756027f2b3872099ffd0bc
Chris Sprague <spragucm@gmail.com>
2022-06-26 14:18:36 -0700

    WIP - trying to use local file and it's not working

--- src/main/kotlin/com/tcubedstudios/angularstudio/terminal/actions/RunCommandInLocalTerminalAction.kt
+++ src/main/kotlin/com/tcubedstudios/angularstudio/terminal/actions/RunCommandInLocalTerminalAction.kt
@@ -1,19 +1,21 @@
 package com.tcubedstudios.angularstudio.terminal.actions
 
+import com.google.common.io.Files
 import com.intellij.ide.plugins.PluginManager
-import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.extensions.PluginId
+import com.intellij.openapi.fileTypes.FileType
+import com.intellij.openapi.fileTypes.FileTypeManager
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.wm.ToolWindowManager
-import com.intellij.terminal.JBTerminalWidget
 import com.tcubedstudios.angularstudio.terminal.utils.getEventProject
 import org.jetbrains.plugins.terminal.ShellTerminalWidget
 import org.jetbrains.plugins.terminal.TerminalToolWindowFactory
 import org.jetbrains.plugins.terminal.TerminalView
 import java.io.IOException
-
+import java.io.File
+import com.intellij.platform.templates.ArchivedTemplatesFactory
 
 // https://intellij-support.jetbrains.com/hc/en-us/community/posts/360005329339-Execute-command-in-the-terminal-from-plugin-action?page=1#community_comment_6186561275794
 // https://github.com/JetBrains/intellij-community/blob/95ab6a1ecfdf49754f7eb5a81984cdc2c4fa0ca5/plugins/sh/src/com/intellij/sh/run/ShTerminalRunner.java
@@ -33,6 +35,14 @@ class RunCommandInLocalTerminalAction: DumbAwareAction() {
             null -> LOGGY.error("Cannot run command in local terminal. Project is null")
             else -> {
                 try {
+                    val file = File("/src/main/kotlin/com.tcubedstudios.angularstudio/scripts/DeleteMe.txt")///src/main/kotlin/com.tcubedstudios.angularstudio/scripts/DeleteMe.txt
+//                    val fileType: FileType = FileTypeManager.getInstance().getFileTypeByFileName("pom.xml")
+                    val content: String = Files.asCharSource(file, Charsets.UTF_8).read()
+                    println(content)
+
+
+
+
                     val pluginPath = PluginManager.getPlugin(PluginId.getId("com.tcubedstudios.angularstudio"))?.path
                     val scriptsRootPath = "$pluginPath\\scripts" //"I:\\Google Drive (HoverDroids)\\Code\\Windows\\Scripts"
                     val scriptPath = "$scriptsRootPath\\Angular\\CreateWorkspaceWithMultipleProjectsAndLibraries.ps1"
