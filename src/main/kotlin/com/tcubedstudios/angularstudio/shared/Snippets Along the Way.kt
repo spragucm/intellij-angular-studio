3f58467bcaf1823df765e1f55dc22b29036d79b1
Chris Sprague <spragucm@gmail.com>
2022-07-26 06:05:47 -0700

    Added a file for copying useful snippets I find

---
+++ src/main/kotlin/com/tcubedstudios/angularstudio/shared/Snippets Along the Way.kt
@@ -0,0 +1,29 @@
+package com.tcubedstudios.angularstudio.shared
+
+import com.intellij.collaboration.util.ProgressIndicatorsProvider
+import com.intellij.openapi.util.Disposer
+import com.intellij.util.ui.UIUtil
+
+/*row {
+           // need some pushx/grow component to test label cell grow policy if there is cell with several components
+           scrollPane(JTextArea())
+       }*/
+
+/*link(JavaRefactoringBundle.message("extract.method.link.label.more.options"), null) { showDialogAction(null) }
+    .applyToComponent { isFocusable = true }
+comment(KeymapUtil.getFirstKeyboardShortcutText(ACTION_EXTRACT_METHOD))*/
+
+
+/*return panel {
+    row {
+        Cell
+        splitter1
+    }
+}*/
+
+/*
+val indicatorsProvider = ProgressIndicatorsProvider().also {
+    Disposer.register(disposable!!, it)
+}*/
+
+
